fac7fd81195574a194d3ed131a74d0505fe96a94
Gonzalo Henriquez <henriquez@wiz.cl>
2019-03-29 11:45:36 -0300

    Cosmetic improvement

--- console/Example.php
+++ console/Example.php
@@ -28,7 +28,7 @@ class Example extends Command
 
         # Work with the request data. You will have access to the hook object in $request->hook
         $this->output->writeln('Executed test console command');
-        $this->output->writeln('Hook: ' . $request->hook->name . ' (hookId: ' . $request->hook_id);
+        $this->output->writeln('Hook: ' . $request->hook->name . ' (hookId: ' . $request->hook_id . ')');
         $this->output->writeln('Received data:');
         $this->output->write(json_encode($request->request_data), true);
     }
